0d74a8c9eac87e61935d606bfa69d2cc49def78d
Adrian <adrianwong@live.ca>
2020-11-07 14:01:24 -0800

    Update PlayerMovement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float moveSpeed = 100;
-
+    public float moveSpeed;
     private Rigidbody2D rb2d;
     void Start () {
         rb2d = GetComponent<Rigidbody2D>();
