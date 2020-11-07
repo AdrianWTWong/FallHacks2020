8413b87617ae0e09d767f0794739c3e608d9a7e4
Adrian <adrianwong@live.ca>
2020-11-07 11:45:43 -0800

    Initial Push
    Uploading TestScene

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rigidbody;
+
+	// Use this for initialization
+	//void Start () {
+ //       rigidbody.AddForce(0, 200, 500);
+	//}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rigidbody.AddForce(0, 0, 2000 * Time.deltaTime);
+		
+	}
+}
